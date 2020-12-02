323022a73cdb008184cddb2fb5444422a73876d6
Myrdin <myrdin@clic2000.fr>
2020-12-03 00:23:04 +0100

    prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,68 @@
 //Lists of characters
-const emailChars = "ab1cdefgh9ijklmnopqrst2uvwxyz8ABCD5EF4GHIJKLMNOPQRSTUVW6XYZ!#$%7&'*+-/=?^_`{|}~.";
-const replacedChars = "ɐʯ7ɑЖ1ЮУʕэʖʗʘʙʢʡʥʂ2ʃ9ʄɸɪɫɬɭяɒɓɔ8ɕɖɗɣɤɥɦɧɨɶɵɴɳɲɱɰɯɮʭʬ5ʫʪʩ6ʨʧʟʞ4ʝʜʛʚΞΦζξψϟϞϛϚϙϐϔϑλ";
+const emailChars =
+  "ab1cdefgh9ijklmnopqrst2uvwxyz8ABCD5EF4GHIJKLMNOPQRSTUVW6XYZ!#$%7&'*+-/=?^_`{|}~.";
+const replacedChars =
+  "ɐʯ7ɑЖ1ЮУʕэʖʗʘʙʢʡʥʂ2ʃ9ʄɸɪɫɬɭяɒɓɔ8ɕɖɗɣɤɥɦɧɨɶɵɴɳɲɱɰɯɮʭʬ5ʫʪʩ6ʨʧʟʞ4ʝʜʛʚΞΦζξψϟϞϛϚϙϐϔϑλ";
 
-function getEmailAndGenerate(){
-    let mailAddress = document.getElementById('email').value;
-    //Encode - Decode 
-    function substitution(emailChars, replacedChars) {
-	this.encode = function (str) {
-	    return str.replace(/./g, x => replacedChars[emailChars.indexOf(x)] || x);
-	};
-	this.decode = function (str) {
-	    return str.replace(/./g, x => emailChars[replacedChars.indexOf(x)] || x);
-	};
-    }
+function getEmailAndGenerate() {
+  let mailAddress = document.getElementById("email").value;
+  //Encode - Decode
+  function substitution(emailChars, replacedChars) {
+    this.encode = function (str) {
+      return str.replace(
+        /./g,
+        (x) => replacedChars[emailChars.indexOf(x)] || x
+      );
+    };
+    this.decode = function (str) {
+      return str.replace(
+        /./g,
+        (x) => emailChars[replacedChars.indexOf(x)] || x
+      );
+    };
+  }
 
-    let sub = new substitution(emailChars, replacedChars);
-    let mailEncoded = sub.encode(mailAddress);
+  let sub = new substitution(emailChars, replacedChars);
+  let mailEncoded = sub.encode(mailAddress);
 
-    //Remove duplicate characters
-    function removeDuplicateCharacters(string) {
-	return string
-	    .split('')
-	    .filter(function (item, pos, self) {
-	    return self.indexOf(item) == pos;
-	})
-	    .join('');
-    }
-    
-    let removedDuplicates = removeDuplicateCharacters(mailEncoded);
+  //Remove duplicate characters
+  function removeDuplicateCharacters(string) {
+    return string
+      .split("")
+      .filter(function (item, pos, self) {
+        return self.indexOf(item) == pos;
+      })
+      .join("");
+  }
 
-    //Shuffle
-    let shuffledCharacters = removedDuplicates.split('').sort(function () { return 0.5 - Math.random(); }).join('');
+  let removedDuplicates = removeDuplicateCharacters(mailEncoded);
 
-    //Decode
-    let charsDecoded = sub.decode(shuffledCharacters);
+  //Shuffle
+  let shuffledCharacters = removedDuplicates
+    .split("")
+    .sort(function () {
+      return 0.5 - Math.random();
+    })
+    .join("");
 
-    //Build our command
-    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(mailAddress.match(mailformat)){
-	document.getElementById('command').innerHTML = "<p> <i class='fa fa-terminal' aria-hidden='true'></i> " +  "This is the command you can share :" +  "</p>"  + "<code>" + `echo '${mailEncoded}' | sed 'y/${shuffledCharacters}/${charsDecoded}/'` + "</code>";
-    } else {
-	document.getElementById('command').innerHTML = "<p>" + "Please enter valid email address." + "</p>";
-    }
+  //Decode
+  let charsDecoded = sub.decode(shuffledCharacters);
+
+  //Build our command
+  let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (mailAddress.match(mailformat)) {
+    document.getElementById("command").innerHTML =
+      "<p> <i class='fa fa-terminal' aria-hidden='true'></i> " +
+      "This is the command you can share :" +
+      "</p>" +
+      "<code>" +
+      `echo '${mailEncoded}' | sed 'y/${shuffledCharacters}/${charsDecoded}/'` +
+      "</code>";
+  } else {
+    document.getElementById("command").innerHTML =
+      "<p>" + "Please enter valid email address." + "</p>";
+  }
 }
 
-let generateButton = document.getElementById('generate');
-generateButton.addEventListener('click', getEmailAndGenerate);
-
-
+let generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", getEmailAndGenerate);
